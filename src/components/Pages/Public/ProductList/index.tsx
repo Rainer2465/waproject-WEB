1f02d1092ab2784f60fbe7ef5f7d7053e34f77ab
Rainer2465 <rainerdaniel9@hotmail.com>
2020-10-18 00:57:38 -0300

    Emergencial commit case: lack of energy

--- src/components/Pages/Public/ProductList/index.tsx
+++ src/components/Pages/Public/ProductList/index.tsx
@@ -1,62 +1,43 @@
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Grid from '@material-ui/core/Grid';
-import makeStyles from '@material-ui/core/styles/makeStyles';
-import Typography from '@material-ui/core/Typography';
 import Toolbar from 'components/Layout/Toolbar';
 import React, { Fragment, memo, useState } from 'react';
-import { Bar, BarChart, ResponsiveContainer, Tooltip } from 'recharts';
 
-const useStyles = makeStyles({
-  marginBottom: {
-    marginBottom: 15
-  }
-});
-
-//* PV = Valor Total Vendido
-//* UV = Total Unidades Vendidas
-const OrderIndexPage = memo((props: {}) => {
-  const classes = useStyles(props);
-  const [dataMes] = useState([
-    { name: 'Janeiro', uv: 4000, pv: 2400 },
-    { name: 'Fevereiro', uv: 3000, pv: 1398 },
-    { name: 'Março', uv: 2000, pv: 9800 },
-    { name: 'Abril', uv: 2780, pv: 3908 },
-    { name: 'Maio', uv: 1890, pv: 4800 },
-    { name: 'Junho', uv: 2390, pv: 3800 },
-    { name: 'Julho', uv: 3490, pv: 4300 },
-    { name: 'Agosto', uv: 4000, pv: 2400 },
-    { name: 'Setembro', uv: 3490, pv: 1398 },
-    { name: 'Outubro', uv: 2000, pv: 9800 },
-    { name: 'Novembro', uv: 1890, pv: 4800 },
-    { name: 'Dezembro', uv: 2390, pv: 4300 }
-  ]);
-
-  const [colors] = useState(['#0088FE', '#00C49F', '#FFBB28', '#FF8042']);
+const OrderIndexPage = memo(() => {
+  const [produto] = useState({
+    name: 'Phone XL',
+    price: 799,
+    description:
+      'Maior desempenho Sua memória RAM de 3 GB, lhe permitirá executar vários aplicativos ao mesmo tempo, jogar e navegar rapidamente e sem inconvenientes. Mais para ver Com sua tela grande IPS de 5.5", desfrute de cores intensas e maior clareza em todo o seu conteúdo.',
+    quantidade: '1'
+  });
 
   return (
     <Fragment>
       <Toolbar title='Produtos' />
-
-      <Grid container spacing={3} className={classes.marginBottom}>
-        <Grid item xs={12} md={5} lg={6}>
-          <Card>
-            <CardContent>
-              <Typography gutterBottom variant='subtitle1'>
-                Total Produto | Valor Vendido - Mês
-              </Typography>
-
-              <ResponsiveContainer width='100%' height={200}>
-                <BarChart data={dataMes}>
-                  <Tooltip />
-                  <Bar dataKey='uv' fill={colors[0]} />
-                  <Bar dataKey='pv' fill={colors[3]} />
-                </BarChart>
-              </ResponsiveContainer>
-            </CardContent>
-          </Card>
-        </Grid>
-      </Grid>
+      <Card>
+        <CardContent>
+          <Grid container justify='space-between' alignItems='center' spacing={2}>
+            <Grid item xs={6} sm={3} lg={3}>
+              <div>
+                <img
+                  src='https://i.zst.com.br/images/smartphone-apple-iphone-6-6-16gb-16gb-8-0-mp-apple-a8-ios-8-photo29396423-12-3d-10.jpg'
+                  width='350'
+                />
+              </div>
+            </Grid>
+            <Grid item xs={1} sm={1} lg={9}>
+              <div>Produto: {produto.name}</div>
+              <div>Preço: R$ {produto.price}</div>
+              <div>Descrição: {produto.description}</div>
+              <div>
+                <input value={produto.quantidade} />
+              </div>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
     </Fragment>
   );
 });
